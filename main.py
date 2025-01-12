75092b93dc84e7aaf33e1bb8e22c7146e157dd61
imgaurav-17 <123211146+imgaurav-17@users.noreply.github.com>
2025-01-12 16:01:20 +0530

    Update main.py
    env update

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 import logging
-import json # for config
+import os
 import requests
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-# Amazon Stuff
-from amazon_paapi import get_asin # Get asin from url
-from utils.create_message import amazon_message # Create HTML template for amazon
+from amazon_paapi import get_asin
+from utils.create_message import amazon_message
 from utils.product_amazon import Product
-from utils.tools import  check_domain
-
-with open('config/credentials.json') as config_file:
-    config = json.load(config_file)
+from utils.tools import check_domain
 
 # Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
 
-
 def start(update, context):
     update.message.reply_text('Send me links from Amazon! I will give you back a nice post.')
-
 
 def message_url(update, context):
 
@@ -39,18 +34,23 @@ def message_url(update, context):
 
         product = Product(get_asin(url))
         message = amazon_message(product, update)
-        context.bot.send_message(update.message.chat_id, message[0] , reply_markup=message[1], parse_mode='HTML')
+        context.bot.send_message(update.message.chat_id, message[0], reply_markup=message[1], parse_mode='HTML')
         context.bot.delete_message(update.message.chat_id, update.message.message_id)
 
-
 def main():
-    updater = Updater(config['BOT-TOKEN'], use_context=True)
+    # Load Telegram BOT-TOKEN from environment variables
+    bot_token = os.getenv('BOT_TOKEN')
 
+    updater = Updater(bot_token, use_context=True)
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
 
-    dispatcher.add_handler(MessageHandler(Filters.regex('(?i)((?:https?://|www\d{0,3}[.])?[a-z0-9.\-]+[.](?:(?:com.br/)|(?:ca/)|(?:com.mx/)|(?:com/)|(?:cn/)|(?:in/)|(?:co.jp/)|(?:sg/)|(?:com.tr/)|(?:ae/)|(?:sa/)|(?:fr/)|(?:de/)|(?:it/)|(?:nl/)|(?:pl/)|(?:es/)|(?:se/)|(?:co.uk/)|(?:com.au/))(?:/[^\s()<>]+[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019])?)'), message_url))
+    dispatcher.add_handler(MessageHandler(Filters.regex('(?i)((?:https?://|www\d{0,3}[.])?[a-z0-9.\-]+[.](?:(?:com.br/)|(?:ca/)|(?:com.mx/)|(?:com/)|(?:cn/)|(?:in/)|(?:co.jp/)|(?:sg/)|(?:com.tr/)|(?:ae/)|(?:sa/)|(?:fr/)|(?:de/)|(?:it/)|(?:nl/)|(?:pl/)|(?:es/)|(?:se/)|(?:co.uk/)|(?:com.au/))(?:/[^\s()<>]+[^\s`!()
+
+\[\]
+
+{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019])?)'), message_url))
 
     updater.start_polling()
     updater.idle()
